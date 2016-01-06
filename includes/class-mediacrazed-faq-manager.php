7f2897d6d707c8c31f462886bc8d9d4c24be2e84
Jared Williams <jaredm.wills@gmail.com>
2016-01-06 09:24:44 -0600

    Update class-mediacrazed-faq-manager.php

--- includes/class-mediacrazed-faq-manager.php
+++ includes/class-mediacrazed-faq-manager.php
@@ -113,12 +113,6 @@ class Mediacrazed_Faq_Manager {
 		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-mediacrazed-faq-manager-admin.php';
 
-		/**
-		 * The class responsible for defining all actions that occur in the public-facing
-		 * side of the site.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-mediacrazed-faq-manager-public.php';
-
 		$this->loader = new Mediacrazed_Faq_Manager_Loader();
 
 	}
@@ -151,26 +145,7 @@ class Mediacrazed_Faq_Manager {
 
 		$plugin_admin = new Mediacrazed_Faq_Manager_Admin( $this->get_plugin_name(), $this->get_version() );
 
-		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
-		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
-
 		$this->loader->add_action( 'init', $plugin_admin, 'mc_faq_custom_post_type' );
-
-	}
-
-	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_public_hooks() {
-
-		$plugin_public = new Mediacrazed_Faq_Manager_Public( $this->get_plugin_name(), $this->get_version() );
-
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
 	}
 
