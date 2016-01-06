4d7dfe554396eacc56366251305e42e076e11418
Jared Williams <jaredm.wills@gmail.com>
2016-01-06 09:21:01 -0600

    Update class-mediacrazed-faq-manager-admin.php

--- admin/class-mediacrazed-faq-manager-admin.php
+++ admin/class-mediacrazed-faq-manager-admin.php
@@ -55,52 +55,6 @@ class Mediacrazed_Faq_Manager_Admin {
 	}
 
 	/**
-	 * Register the stylesheets for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Mediacrazed_Faq_Manager_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Mediacrazed_Faq_Manager_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/mediacrazed-faq-manager-admin.css', array(), $this->version, 'all' );
-
-	}
-
-	/**
-	 * Register the JavaScript for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Mediacrazed_Faq_Manager_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Mediacrazed_Faq_Manager_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/mediacrazed-faq-manager-admin.js', array( 'jquery' ), $this->version, false );
-
-	}
-
-	/**
 	 * Register the FAQ Custom Post Type for the admin area.
 	 *
 	 * @since    1.0.0
