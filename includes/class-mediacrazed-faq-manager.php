944f132d2772a0b331716cafc4a22e6c282256df
Jared Williams <jaredm.wills@gmail.com>
2016-01-12 13:04:43 -0600

    Update class-mediacrazed-faq-manager.php

--- includes/class-mediacrazed-faq-manager.php
+++ includes/class-mediacrazed-faq-manager.php
@@ -171,6 +171,7 @@ class Mediacrazed_Faq_Manager {
 
 		$plugin_public = new Mediacrazed_Faq_Manager_Public( $this->get_plugin_name(), $this->get_version() );
 
+		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
 	}
